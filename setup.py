11df976e8c6fccf70dac29746e92f66aad375162
daegyu hwang <daegyu@telelian.com>
2024-08-28 13:55:57 +0900

    init

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'tbot_gazebo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name, 'models/turtlebot_waffle_gps'),
+         glob('models/turtlebot_waffle_gps/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dghwang',
+    maintainer_email='daegyu@telelian.com',
+    description='tbot gazebo',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
